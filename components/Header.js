133483091784237295076ae8d0e57343ac8e843c
Calvin Teater <laughingglint3@gmail.com>
2022-07-26 20:18:16 -0500

    add cursor features and icon spacing

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons
 
 export default function Header() {
   return (
-    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
         <div className="flex">
             <HeaderIcon Icon={HomeIcon} title="HOME"/>
             <HeaderIcon Icon={UserIcon} title="ACCOUNT"/>
@@ -17,6 +17,7 @@ export default function Header() {
        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/440px-IMDB_Logo_2016.svg.png"
         width={100}
         height={100}
+        className="cursor-pointer active:bright-110"
         />
     </div>
   )
